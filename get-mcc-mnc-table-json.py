9cdccc7f2c90c0b60f2ebe6e3f71bf1b67c97a89
Ong Hui Sheng <deathezer10@live.com.sg>
2023-07-10 00:04:45 +0800

    chore: rebased forked commits

--- get-mcc-mnc-table-json.py
+++ get-mcc-mnc-table-json.py
@@ -1,84 +1,175 @@
-import re
+"""
+This module can be used to fetch & parse mcc, mnc data from https://mcc-mnc.com
+Usage:
+    from mcc_mnc_fetcher import MCCMNCFetcher
+    MCCMNCFetcher.json_file()  # creates mcc-mnc-table.json
+    MCCMNCFetcher.csv_file()   # creates mcc-mnc-table.cxv
+    MCCMNCFetcher.xml_file()   # creates mcc-mnc-table.xml
+"""
+import csv
 import json
+
+import pydantic
 import requests
+import xml.etree.ElementTree as ET
 from typing import List
+from pathlib import Path
+from xml.dom import minidom
+from bs4 import BeautifulSoup
+from pydantic import BaseModel, RootModel
 from unidecode import unidecode
 
-DOMAIN: str = "https://mcc-mnc-list.com"
-MAIN_JS_FILE: str = "main.0744a96a58a20d0b.js"
-JSON_PATTERN: re.Pattern = re.compile(r"JSON.parse\('(\[.*\])'\),")  # very specific pattern, don't use it elsewhere XD
+_MCC_MNC_FILE: Path = Path(__file__).parent.joinpath("mcc-mnc-table")
 
-def create_dart_file(json_object: any):    
-        mcc_mcc_map = {}    
-        
-        for entry in json_object:
-            
+
+# SCHEMAS
+class MCCMNCRecord(BaseModel):
+    """mcc mnc record/row from mcc-mnc.com"""
+    mcc: int
+    mnc: int
+    iso: str
+    country: str
+    country_code: int
+    network: str
+
+
+class MCCMNCRecordList(RootModel):
+    root: List[MCCMNCRecord]
+
+
+# FETCHER
+class MCCMNCFetcher:
+    """fetching & parsing mcc mnc records from https://mcc-mnc.com"""
+    _DOMAIN: str = "https://mcc-mnc.com/"
+
+    @staticmethod
+    def _get_mcc_mnc_site_html() -> str:
+        """:return: HTML from https://mcc-mnc.com/"""
+        return requests.get(MCCMNCFetcher._DOMAIN).text
+
+    @staticmethod
+    def _get_mcc_mnc_rows(html: str) -> List[List[str]]:
+        """
+        returns all the mcc mnc rows from https://mcc-mnc.com/
+        each row is in the pattern [mcc, mnc, iso, country, country code, network]
+        :param html: HTML from https://mcc-mnc.com/
+        :return: list of mcc mnc rows
+        """
+        records = []
+        soup = BeautifulSoup(html)
+        table = soup.find('table', attrs={'id': 'mncmccTable'})
+        table_body = table.find('tbody')
+        rows = table_body.find_all('tr')
+        for row in rows:
+            cols = row.find_all('td')
+            cols = [e.text.strip() for e in cols]
+            records.append([e for e in cols if e])
+        return records
+
+    @staticmethod
+    def _parse_records(mcc_mnc_rows: List[List[str]]) -> List[MCCMNCRecord]:
+        """
+        parsing mcc mnc rows to list of MCCMNCRecord
+        :param mcc_mnc_rows: list of mcc mnc rows. row is in the pattern [mcc, mnc, iso, country, country code, network]
+        :return: list of MCCMNCRecord
+        """
+        mcc_mnc_records: List[MCCMNCRecord] = []
+        for mcc_mnc_row in mcc_mnc_rows:
+            try:
+                mcc_mnc_records.append(
+                    MCCMNCRecord(mcc=mcc_mnc_row[0],
+                                 mnc=mcc_mnc_row[1],
+                                 iso=mcc_mnc_row[2],
+                                 country=mcc_mnc_row[3],
+                                 country_code=mcc_mnc_row[4],
+                                 network=mcc_mnc_row[5])
+                )
+            except IndexError:
+                pass  # some rows are not fully populated
+        return mcc_mnc_records
+
+    @staticmethod
+    def json_file() -> None:
+        """
+        creating a JSON file (mcc-mnc-table.json) with all the MCC MNC data from https://mcc-mnc.com/
+        :return: None
+        """
+        mcc_mnc_html: str = MCCMNCFetcher._get_mcc_mnc_site_html()
+        mcc_mnc_rows: List[List[str]] = MCCMNCFetcher._get_mcc_mnc_rows(html=mcc_mnc_html)
+        mcc_mnc_records: List[MCCMNCRecord] = MCCMNCFetcher._parse_records(mcc_mnc_rows=mcc_mnc_rows)
+        mcc_mnc_record_list = MCCMNCRecordList(mcc_mnc_records)
+
+        with open("mcc-mnc-table.json", "w") as f:
+            json.dump(mcc_mnc_record_list.model_dump(), f, indent=2)
+
+    @staticmethod
+    def csv_file() -> None:
+        """
+        creating a csv file(mcc-mnc-table.csv) with all the mcc mnc data from https://mcc-mnc.com/
+        :return: None
+        """
+        mcc_mnc_html: str = MCCMNCFetcher._get_mcc_mnc_site_html()
+        mcc_mnc_rows: List[List[str]] = MCCMNCFetcher._get_mcc_mnc_rows(html=mcc_mnc_html)
+        with open(f"{_MCC_MNC_FILE}.csv", "w", newline="") as f:
+            csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            for mcc_mnc_row in mcc_mnc_rows:
+                csv_writer.writerow(mcc_mnc_row)
+
+    @staticmethod
+    def xml_file() -> None:
+        """
+        creating a xml file(mcc-mnc-table.xml) with all the mcc mnc data from https://mcc-mnc.com/
+        :return: None
+        """
+        mcc_mnc_html: str = MCCMNCFetcher._get_mcc_mnc_site_html()
+        mcc_mnc_row_pattern = ["mcc", "mnc", "iso", "country", "country_code", "network"]
+        mcc_mnc_rows: List[List[str]] = MCCMNCFetcher._get_mcc_mnc_rows(html=mcc_mnc_html)
+        root_tag = ET.Element('records')
+        for mcc_mnc_row in mcc_mnc_rows:
+            record_tag = ET.SubElement(root_tag, 'record')
+            for index, value in enumerate(mcc_mnc_row):
+                vary_tag = ET.SubElement(record_tag, mcc_mnc_row_pattern[index])
+                vary_tag.text = value
+        xml_string = minidom.parseString(ET.tostring(root_tag)).toprettyxml()
+        with open(f"{_MCC_MNC_FILE}.xml", "w") as f:
+            f.write(xml_string)
+
+
+def create_dart_file():
+    mcc_mcc_map = {}
+
+    with open("mcc-mnc-table.json", "r") as file:
+        # Load the JSON data
+        data = json.load(file)
+
+        for entry in data:
+
             mcc = ''
             mnc = ''
             operator = ''
-            
+
             if entry['mcc']:
-                mcc = unidecode(entry['mcc'])
+                mcc = entry['mcc']
             if entry['mnc']:
-                mnc = unidecode(entry['mnc'])
-            if entry['operator']:
-                operator = unidecode(entry['operator'])
-            
-            if mcc.isdigit() and mnc.isdigit():
-                mcc = int(mcc)
-                mnc = int(mnc)
-                            
-            if not operator and entry['brand']:
-                operator = unidecode(entry['brand'])
-            
+                mnc = entry['mnc']
+            if entry['network']:
+                operator = unidecode(entry['network'])
+
             mcc_mcc_map[f"{mcc}:{mnc}"] = operator
-            
+
         with open('mobile_network_code.dart', 'w') as f:
-            
+
             f.write("const Map<String, String> MccMncMap = {\n")
-                        
-            for key, value in mcc_mcc_map.items():                
+
+            for key, value in mcc_mcc_map.items():
                 try:
                     f.write(f"      \"{key}\": \"{value}\",\n")
                 except Exception:
                     print(f"Found exception for: \"{key}\": \"{value}\"")
-            
+
             f.write("};")
-
-def get_json_object(url: str = f'{DOMAIN}/{MAIN_JS_FILE}') -> any:
-        scraped_string = ""
-        with requests.get(url, stream=True) as r:
-            r.raise_for_status()  # returns an HTTPError object if an error has occurred during the process.
-            json_string = JSON_PATTERN.findall(r.text)[0]  # finds only 1
-            json_string = clean_json(json_string)
-            scraped_string += prettify_json(json_string)
-            
-        return json.loads(scraped_string)
-    
-def prettify_json(json_data: str) -> str:
-    json_object = json.loads(json_data)
-    return json.dumps(json_object, indent=2, ensure_ascii=False)
-
-
-def clean_json(json_data: str) -> str:
-    """removing problematic unicode/combinations from received json string"""
-    # removing garbage
-    json_data = re.sub(r'"\\\\(?!")', '', json_data)
-    json_data = re.sub(r'\\\\"', '', json_data)
-    json_data = re.sub(r"\\\\n", '`', json_data)
-    json_data = re.sub(r"\\'", '`', json_data)
-    # fix un-decoded unicode objects
-    for unicode in get_list_of_unicode(json_data):
-        json_data = re.sub(rf'\{unicode}', eval(f"str('{unicode}')"), json_data)
-    return json_data
-
-
-def get_list_of_unicode(json_data: str) -> List[str]:
-    """return lists of problematic unicode symbols existing in received json string"""
-    unicode_set = set(re.findall(r"(\\x.{2,}?)", json_data))
-    return list(unicode_set)
 
 
 if __name__ == '__main__':
-    create_dart_file(get_json_object())
-
+    MCCMNCFetcher.json_file()
+    create_dart_file()
